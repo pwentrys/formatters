a6eb4c88ee375f585755f57103555c0e7be71f31
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-07-23 07:01:43 -0400

    Run pep8 on all files in parent folder (where ALL code lives).

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from scripts import Scripts
 import sys
+from pathlib import Path
 
 
 if __name__ == '__main__':
     # Project path.
     path = sys.path[0]
-    Scripts.run(path)
+    Scripts.run(str(Path(path).parent))
