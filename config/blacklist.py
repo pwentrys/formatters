0d7df31e5747d9bffb721e8e3cf73cd975181394
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-07-23 03:31:03 -0400

    Blacklist.

---
+++ config/blacklist.py
@@ -0,0 +1,19 @@
+BEGINS = ['.git', '.idea']
+CONTAINS = ['__pycache__']
+ENDS = ['routes.py']
+
+
+def allowed(string: str) -> bool:
+    for begins in BEGINS:
+        if string.startswith(begins):
+            return False
+
+    for contains in CONTAINS:
+        if string.__contains__(contains):
+            return False
+
+    for ends in ENDS:
+        if string.endswith(ends):
+            return False
+
+    return True
